07eeb37c5c7fd5ec9d687be15f86ae70f44ca41d
WellKochi <WellKochi@Kochis-MBA.local>
2016-11-03 10:50:35 -0400

    Midterm

---
+++ js/WAlc.js
@@ -0,0 +1,87 @@
+InitChart();
+
+function InitChart() {
+
+  var barData = [{
+    'x': "Barely drinking on weekends",
+    'y': 151
+  }, {
+    'x': "Seldom drinking on weekends",
+    'y': 85
+  }, {
+    'x': "Average drinking on weekends",
+    'y': 80
+  }, {
+    'x': "Often drinking on weekends",
+    'y': 51
+  }, {
+    'x': "Always drinking on weekends",
+    'y': 28
+  }];
+
+  var vis = d3.select('#visualisation'),
+    WIDTH = 1000,
+    HEIGHT = 500,
+    MARGINS = {
+      top: 20,
+      right: 20,
+      bottom: 20,
+      left: 50
+    },
+    xRange = d3.scale.ordinal().rangeRoundBands([MARGINS.left, WIDTH - MARGINS.right], 0.1).domain(barData.map(function (d) {
+      return d.x;
+    })),
+
+
+    yRange = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([0,
+      d3.max(barData, function (d) {
+        return d.y;
+      })
+    ]),
+
+    xAxis = d3.svg.axis()
+      .scale(xRange)
+      .tickSize(5)
+      .tickSubdivide(true),
+
+    yAxis = d3.svg.axis()
+      .scale(yRange)
+      .tickSize(5)
+      .orient("left")
+      .tickSubdivide(true);
+
+  vis.append('svg:g')
+    .attr('class', 'x axis')
+    .attr('transform', 'translate(0,' + (HEIGHT - MARGINS.bottom) + ')')
+    .call(xAxis);
+
+  vis.append('svg:g')
+    .attr('class', 'y axis')
+    .attr('transform', 'translate(' + (MARGINS.left) + ',0)')
+    .call(yAxis);
+
+  vis.selectAll('rect')
+    .data(barData)
+    .enter()
+    .append('rect')
+    .attr('x', function (d) {
+      return xRange(d.x);
+    })
+    .attr('y', function (d) {
+      return yRange(d.y);
+    })
+    .attr('width', xRange.rangeBand())
+    .attr('height', function (d) {
+      return ((HEIGHT - MARGINS.bottom) - yRange(d.y));
+    })
+    .attr('fill', 'steelBlue')
+    .on('mouseover',function(d){
+      d3.select(this)
+        .attr('fill','brown');
+  })
+    .on('mouseout',function(d){
+      d3.select(this)
+        .attr('fill','steelBlue');
+    });
+
+}
